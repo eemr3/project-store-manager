679a1d9ed459faae0a8e549366a4b3a4afe9a553
Emerson <eemr3@yahoo.com.br>
2022-03-31 16:16:27 -0300

    feat: Rescue
    adiciona o framework rescue

--- routes/ProductsRoute.js
+++ routes/ProductsRoute.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const ProductsController = require('../controllers/ProductsController');
 const validateProducts = require('../middlewares/ProductsMiddleware');
 
 const routes = express.Router();
 
-routes.get('/', ProductsController.getAll);
+routes.get('/', rescue(ProductsController.getAll));
 
-routes.get('/:id', ProductsController.getById);
+routes.get('/:id', rescue(ProductsController.getById));
 
-routes.post('/', validateProducts, ProductsController.create);
+routes.post('/', validateProducts, rescue(ProductsController.create));
 
-routes.put('/:id', validateProducts, ProductsController.update);
+routes.put('/:id', validateProducts, rescue(ProductsController.update));
+
 module.exports = routes;
