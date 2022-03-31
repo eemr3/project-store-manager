fbb42685555fedc9fa1d002778b6852d508bae3a
Emerson <eemr3@yahoo.com.br>
2022-03-31 20:40:54 -0300

    refact: refatora lógica de validação de sales

--- middlewares/SalesMiddleware.js
+++ middlewares/SalesMiddleware.js
@@ -2,11 +2,11 @@ const SalesSchima = require('../schimas/SalesSchimas');
 
 const validateSales = (req, res, next) => {
   const { error } = SalesSchima.validate(req.body[0]);
-  if (error.message.includes('is required')) {
+  if (error && error.message.includes('is required')) {
     return res.status(400).json({ message: error.message });
   }
 
-  if (error.message.includes('must be')) {
+  if (error && error.message.includes('must be')) {
     return res.status(422).json({ message: error.message });
   }
 
