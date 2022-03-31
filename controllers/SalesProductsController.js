4e02094b5efe58bccf9d241798e6ef2348c84ac2
Emerson <eemr3@yahoo.com.br>
2022-03-31 14:20:49 -0300

    feat: :art: SalesProductsController.js

--- controllers/SalesProductsController.js
+++ controllers/SalesProductsController.js
@@ -1,26 +1,25 @@
 const SalesProductsService = require('../services/SalesProductsService');
 
 const getAll = async (req, res) => {
-  try {
     const result = await SalesProductsService.getAll();
-
     return res.status(200).json(result);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
-  }
 };
 
 const getById = async (req, res) => {
-  try {
     const { id } = req.params;
     const result = await SalesProductsService.getById(Number(id));
     return res.status(200).json(result);
-  } catch (error) {
-    return res.status(404).json({ message: error.message });
-  }
 };
 
+const update = async (req, _res) => {
+  // const { id } = req.params;
+  const { productId, quantity } = req.body[0];
+
+  console.log({ productId, quantity });
+  // return res.status(200).json({ productId, quantity });
+};
 module.exports = {
   getAll,
   getById,
+  update,
 };
