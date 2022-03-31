3066d997b8b667442f5e5a11910dad2c46db62f3
Emerson <eemr3@yahoo.com.br>
2022-03-31 20:40:34 -0300

    feat: :ambulance: SalesProductsRoute.js
    criar rota para para SalesProducts

--- routes/SalesProductsRoute.js
+++ routes/SalesProductsRoute.js
@@ -7,10 +7,7 @@ const routes = express.Router();
 routes.get('/', SalesProductsController.getAll);
 routes.get('/:id', SalesProductsController.getById);
 
-routes.post('/', validateSale, (req, res) => {
-  const { saleId, productId, quantity } = req.body[0];
-  return res.status(201).json({ saleId, productId, quantity });
-});
+routes.post('/', validateSale, SalesProductsController.create);
 
 routes.put('/:id', validateSale, SalesProductsController.update);
 
