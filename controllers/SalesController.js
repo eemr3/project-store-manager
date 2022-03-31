0dca8a3e338551d2d7766108b4c7ebf86a131f0b
Emerson <eemr3@yahoo.com.br>
2022-03-31 14:20:18 -0300

    feat: :art: SalesController.js
    adiciona create e update no controller

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -1,22 +1,14 @@
 const SalesService = require('../services/SalesService');
 
 const getAll = async (req, res) => {
-  try {
     const result = await SalesService.getAll();
     return res.status(200).json(result);
-  } catch (error) {
-    res.status(400).send(error.message);
-  }
 };
 
 const getById = async (req, res) => {
-  try {
     const { id } = req.params;
     const sale = await SalesService.getById(Number(id));
     res.status(200).json(sale);
-  } catch (error) {
-    return res.satatus(400).send(error.message);
-  }
 };
 
 module.exports = {
